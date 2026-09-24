45f81d2cf81a9d3f891e89b0026b68f1233eacb8
otto-link <otto-link@users.noreply.github.com>
2026-09-24 14:07:09 +0000

    deploy: 3363eca639d16c697ab5631d7c27d1b39e84c9d8

--- classhmap_1_1QuadSurfaceFunction.js
+++ classhmap_1_1QuadSurfaceFunction.js
@@ -1,6 +1,6 @@
 var classhmap_1_1QuadSurfaceFunction =
 [
-    [ "QuadSurfaceFunction", "classhmap_1_1QuadSurfaceFunction.html#a618365ddfd48eb8b9064324dfd30f39a", null ],
+    [ "QuadSurfaceFunction", "classhmap_1_1QuadSurfaceFunction.html#a2dd16865514e3936681a02dbde3afe89", null ],
     [ "c00", "classhmap_1_1QuadSurfaceFunction.html#addf076e019756240d02b7b44ddb6296a", null ],
     [ "c10", "classhmap_1_1QuadSurfaceFunction.html#aa28379363663e36a50ab216d4b9434a4", null ],
     [ "c01", "classhmap_1_1QuadSurfaceFunction.html#af648deecae67fcf7c943c3f0198557f8", null ],
--- search/functions_10.js
+++ search/functions_10.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['quad_5fsurface_0',['quad_surface',['../namespacehmap.html#a96d1e72b568ef5f418db70b81ea42ffe',1,'hmap']]],
-  ['quadsurfacefunction_1',['QuadSurfaceFunction',['../classhmap_1_1QuadSurfaceFunction.html#a618365ddfd48eb8b9064324dfd30f39a',1,'hmap::QuadSurfaceFunction']]],
+  ['quadsurfacefunction_1',['QuadSurfaceFunction',['../classhmap_1_1QuadSurfaceFunction.html#a2dd16865514e3936681a02dbde3afe89',1,'hmap::QuadSurfaceFunction']]],
   ['quantize_2',['quantize',['../namespacehmap.html#a826ce248be96455e59c2caa582836eb2',1,'hmap::quantize(const Array &amp;array, int nlevels, float vmin, float vmax)'],['../namespacehmap.html#acfacd1af8f909589d58808bb13f7e462',1,'hmap::quantize(const Array &amp;array, int nlevels)']]],
   ['quilting_3',['quilting',['../namespacehmap.html#ada12bd91a1719f64ff4b652bf1462d2a',1,'hmap']]],
   ['quilting_5fblend_4',['quilting_blend',['../namespacehmap.html#a8407611dc51d0372c4626c6c6fa94acd',1,'hmap']]],
